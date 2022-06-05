7bf7ff0dad0e319a36468ffbf529584ec25dc55d
流徵 <106909228+LiuZhiDev@users.noreply.github.com>
2022-06-05 18:18:44 +0800

    Add files via upload

---
+++ 交互式音乐播放器/播放器窗体/MVC类/中间件_文件浏览器.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace 交互式音乐播放器.文件浏览中间件
+{
+    /// <summary>
+    /// 管理文件浏览器中，各个音频文件的文件信息存放
+    /// </summary>
+    public class 文件信息
+    {
+        /// <summary>
+        /// 程序支持的所有文件类型
+        /// </summary>
+        public enum 项目类型
+        {
+            未知,
+            文件夹,
+            受支持的音频文件,
+            MP3文件,
+            OGG文件,
+            WAV文件,
+            配置文件
+        }
+        /// <summary>
+        /// 设置该文件的文件类型
+        /// </summary>
+        public 项目类型 文件类型 { get; set; } = 项目类型.未知;
+        /// <summary>
+        /// 设置该文件或文件夹的完整路径
+        /// </summary>
+        public string 完整路径 { get; set; }
+        /// <summary>
+        /// 设置该文件或文件夹显示的名称
+        /// </summary>
+        public string 显示名称 { get; set; }
+    }
+
+    /// <summary>
+    /// 完成文件浏览器与系统的交互操作
+    /// </summary>
+    public class 系统交互
+    {
+        [DllImport("shell32.dll")]
+        static extern IntPtr ShellExecute(
+        IntPtr hwnd,
+        string lpOperation,
+        string lpFile,
+        string lpParameters,
+        string lpDirectory,
+        ShowCommands nShowCmd);
+        private enum ShowCommands : int
+        {
+            SW_HIDE = 0,
+            SW_SHOWNORMAL = 1,
+            SW_NORMAL = 1,
+            SW_SHOWMINIMIZED = 2,
+            SW_SHOWMAXIMIZED = 3,
+            SW_MAXIMIZE = 3,
+            SW_SHOWNOACTIVATE = 4,
+            SW_SHOW = 5,
+            SW_MINIMIZE = 6,
+            SW_SHOWMINNOACTIVE = 7,
+            SW_SHOWNA = 8,
+            SW_RESTORE = 9,
+            SW_SHOWDEFAULT = 10,
+            SW_FORCEMINIMIZE = 11,
+            SW_MAX = 11
+        }
+        /// <summary>
+        /// 打开系统的文件资源管理器，然后定位到某个特定的文件或文件夹
+        /// </summary>
+        /// <param name="完整路径"></param>
+        public void 定位文件(string 完整路径)
+        {
+            if (Directory.Exists(完整路径))
+            {
+                ShellExecute(IntPtr.Zero, "open", "explorer.exe", 完整路径, "", ShowCommands.SW_NORMAL);
+                return;
+            }
+
+            ShellExecute(IntPtr.Zero, "open", "explorer.exe", @"/e,/select," + 完整路径, "", ShowCommands.SW_NORMAL);
+
+        }
+
+
+    }
+}
---
+++ 交互式音乐播放器/播放器窗体/设置.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 交互式音乐播放器.设置类
+{
+    /// <summary>
+    /// 管理软件的主要设定
+    /// </summary>
+    internal class 设置
+    {
+        /// <summary>
+        /// 设置软件启动后默认导航到的位置，默认为“音乐”文件夹
+        /// </summary>
+        public static string 默认启动位置 { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+        public static int UI更新时间_粗略 { get; set; } = 400;
+        public static int UI更新时间_精准 { get; set; } = 100;
+        public static int UI更新时间_实时 { get; set; } = 100;
+        public static int 默认BPM { get; set; } = 60;
+        public static int 默认节拍 { get; set; } = 4;
+        public static bool 日志开关 { get; set; } = true;
+        public static 语言.音频类型.循环类型 循环模式 = 语言.音频类型.循环类型.不可循环;
+
+    }
+
+    /// <summary>
+    /// 管理软件运行时的设定
+    /// </summary>
+    internal class 运行时
+    {
+        /// <summary>
+        /// 设置应用程序主界面当前访问的文件夹路径
+        /// </summary>
+        public string 当前文件夹路径 { get; set; } = "";
+        /// <summary>
+        /// 应用程序已经访问过的路径集
+        /// </summary>
+        public List<string> 访问过的文件夹路径 { get; set; } = new List<string>();
+        /// <summary>
+        /// Mp3未匹配到封面，需要执行二次匹配的开关
+        /// </summary>
+        public static bool 本Mp3未匹配到封面 { get; set; } = false;
+        /// <summary>
+        /// 子提示窗的状态
+        /// </summary>
+        public static bool 提示窗打开 { get; set; } = false;
+        public static bool 进度条拉动 { get; set; } = false;
+        public static int 音量 { get; set; } = 100;
+    }
+
+    internal class 日志
+    {
+        /// <summary>
+        /// 向系统输出一个日志
+        /// </summary>
+        /// <param name="日志"></param>
+        public static void 输出(object 日志)
+        {
+            if (设置.日志开关 == false) { return; }
+
+            Console.WriteLine($"{DateTime.Now}|{日志.ToString()}");
+        }
+
+    }
+}
